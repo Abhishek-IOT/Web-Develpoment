cedefc00316614d51ec34956179d627ec894e40f
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-11-30 20:10:49 +0530

    Added Docstring to a Function

--- New folder/Dev/Dev/Views.py
+++ New folder/Dev/Dev/Views.py
@@ -3,4 +3,7 @@ def index(request):
     return HttpResponse('''"<h1>My name is Abhishek</h1><a href="https://www.youtube.com/"> Code With Abhishek </a>''')
 
 def Make(request):
+    """Just for making a Pipeline
+    //  http://127.0.0.1:8000/Make=This will open a Page with just Make
+    """
     return HttpResponse("Make")
