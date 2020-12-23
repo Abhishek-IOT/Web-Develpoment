283f2cb2140dca701dd7ebe998a9b81928653718
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-12-23 20:03:15 +0530

    Having the revision so added and deleted some files and now going to make a textutil website

--- New folder/Dev/Dev/Views.py
+++ New folder/Dev/Dev/Views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
 def index(request):
-    return HttpResponse('''"<h1>My name is Abhishek</h1><a href="https://www.youtube.com/"> Code With Abhishek </a>''')
+    return render(request,"Insert.html")
+
 
 def Make(request):
     """Just for making a Pipeline
     //  http://127.0.0.1:8000/Make=This will open a Page with just Make
     """
     return HttpResponse("Make<a href='/'>back</a>")
+
+
