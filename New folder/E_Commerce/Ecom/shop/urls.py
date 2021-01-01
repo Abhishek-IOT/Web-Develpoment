5cca1d3d1c75473f59daa4d7be7512a1095b83a4
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-01 15:47:02 +0530

    Made the end points of my Ecommerce website and further will add the functionality

--- New folder/E_Commerce/Ecom/shop/urls.py
+++ New folder/E_Commerce/Ecom/shop/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index,name="Shop Home")
+    path("",views.index,name="Shop Home"),
+    path("contact/",views.Contact,name="ContactUs"),
+    path("tracker/",views.Tracker,name="Track Your Order"),
+    path("search/",views.Search,name="Search"),
+    path("productviews/",views.prodview,name="Product View"),
+    path("about/",views.about,name="About Us"),
+    path("Checkout/",views.check,name="CheckOut")
 ]
--- New folder/E_Commerce/Ecom/shop/views.py
+++ New folder/E_Commerce/Ecom/shop/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'Shop/index.html')
+def about(request):
+    pass
+def Tracker(request):
+    pass
+def Search(request):
+    pass
+def prodview(request):
+    pass
+def check(request):
+    pass
+def Contact(request):
+    pass
