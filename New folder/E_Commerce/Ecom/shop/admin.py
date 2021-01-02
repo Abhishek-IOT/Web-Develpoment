99a05413c566c104783f5ae33fb366a7f36e153f
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-02 08:48:33 +0530

    description of the commands used

--- New folder/E_Commerce/Ecom/shop/admin.py
+++ New folder/E_Commerce/Ecom/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+#TO create a admin we will use python manage.py createsuperuser
 # Register your models here.
 from . models import  Product
 admin.site.register(Product)
