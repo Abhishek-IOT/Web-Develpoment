9e600a4706446da791b60d067baab5ff3e58645d
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2021-01-09 20:38:50 +0530

    done the changes in the following and added more products using admin

--- New folder/E_Commerce/Ecom/shop/views.py
+++ New folder/E_Commerce/Ecom/shop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'Shop/index.html')
+    product=Product.objects.all()
+    print(product)
+    n=len(product)
+    nslide=n//4+ceil((n/4)-(n//4))
+    params = {'no_of_sildes':nslide,'range':range(1,nslide),'product': product}
+    return render(request,'Shop/index.html',params)
 def about(request):
     return render(request,'Shop/about.html')
 def Tracker(request):
